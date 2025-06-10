e311de68064e76d08441ad5ce0bc212517f2dbb3
DeabTest <carl@destinationeskilstuna.se>
2025-06-10 08:29:13 +0200

    Update rss-proxy.js

--- netlify/functions/rss-proxy.js
+++ netlify/functions/rss-proxy.js
@@ -1,1 +1,22 @@
+export async function handler(event, context) {
+  const targetUrl = "https://visiteskilstuna.se/rest-api/rssService/getRssFeed?portletId=12.5b899160171ac5273c72ad9f&pageId=2.369be3c31580a19562d1977e&t=" + Date.now();
 
+  try {
+    const response = await fetch(targetUrl);
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "text/xml",
+      },
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Kunde inte hämta RSS-flödet.",
+    };
+  }
+}
